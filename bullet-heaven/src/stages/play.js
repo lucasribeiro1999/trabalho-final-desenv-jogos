d47d9a5acf889163e51923a009ec99cb9159575e
emanuelpna <emanuelpna@yahoo.com.br>
2025-11-06 11:23:43 -0300

    adiciona movimento do player em todas as direções

--- bullet-heaven/src/stages/play.js
+++ bullet-heaven/src/stages/play.js
@@ -20,9 +20,13 @@ class PlayScreen extends me.Stage {
 
 
         me.input.bindKey(me.input.KEY.LEFT, "left");
-        me.input.bindKey(me.input.KEY.RIGHT, "right");
         me.input.bindKey(me.input.KEY.A, "left");
+        me.input.bindKey(me.input.KEY.RIGHT, "right");
         me.input.bindKey(me.input.KEY.D, "right");
+        me.input.bindKey(me.input.KEY.UP, "up");
+        me.input.bindKey(me.input.KEY.W, "up");
+        me.input.bindKey(me.input.KEY.DOWN, "down");
+        me.input.bindKey(me.input.KEY.S, "down");
 
         me.input.bindKey(me.input.KEY.SPACE, "shoot", true);
     }
