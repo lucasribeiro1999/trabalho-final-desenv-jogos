db148a802e5047e8cf63b42d638f4273d270885f
emanuelpna <emanuelpna@yahoo.com.br>
2025-11-08 16:49:16 -0300

    move trigger de animação de morte do zumbi para um método e corrije cálculo de posição do player

--- bullet-heaven/src/renderables/enemy.js
+++ bullet-heaven/src/renderables/enemy.js
@@ -1,5 +1,8 @@
 import * as me from 'melonjs';
+
 import PlayerEntity from './player.js';
+
+import CONSTANTS from '../constants.js';
 
 class EnemyEntity extends me.Sprite {
     constructor(x, y) {
@@ -9,12 +12,12 @@ class EnemyEntity extends me.Sprite {
             frameheight: 18,
         });
 
-        // give the sprite a physics body so it can collide and stuff
         this.body = new me.Body(this);
         this.body.addShape(new me.Rect(0, 0, this.width, this.height));
         this.body.collisionType = me.collision.types.ENEMY_OBJECT;
         this.body.ignoreGravity = true;
-        // make enemy immovable by physics responses; we move it manually
+
+
         if (this.body.setStatic) this.body.setStatic(true);
         this.body.vel.set(0, 0);
         this.body.force.set(0, 0);
@@ -26,13 +29,34 @@ class EnemyEntity extends me.Sprite {
             );
         }
 
-        // randomize and set animation
         this.addAnimation("idle", [0, 1, 2, 3], 350);
         this.setCurrentAnimation("idle");
 
-        this.scale(3)
+        this.scale(CONSTANTS.SPRITE.SCALE_UP)
         this.speed = 80;
         this.isDying = false;
+    }
+
+    startDeath() {
+        if (this.isDying) return;
+        this.isDying = true;
+        if (this.body.setCollisionMask) this.body.setCollisionMask(0);
+
+        const parent = this.ancestor || me.game.world;
+
+        const deathSprite = new me.Sprite(this.pos.x, this.pos.y, {
+            image: me.loader.getImage("zombie-axe-death"),
+            framewidth: 27,
+            frameheight: 18,
+        });
+        deathSprite.addAnimation("death", [0, 1, 2, 3, 4, 5], 100);
+        deathSprite.setCurrentAnimation("death", () => {
+            parent.removeChild(deathSprite);
+        });
+        deathSprite.scale(CONSTANTS.SPRITE.SCALE_UP);
+        parent.addChild(deathSprite, (typeof this.z === 'number' ? this.z : 2));
+
+        if (parent) parent.removeChild(this);
     }
 
     /**
@@ -42,20 +66,10 @@ class EnemyEntity extends me.Sprite {
      */
     onCollision(response, other) {
         if (other.body.collisionType === me.collision.types.PROJECTILE_OBJECT) {
-            if (this.isDying) return false;
-            this.isDying = true;
-            // swap to death spritesheet and play once, then remove
-            this.setImage(me.loader.getImage("zombie-axe-death"), 27, 18);
-            this.addAnimation("death", [0, 1, 2, 3, 4, 5], 100);
-            this.setCurrentAnimation("death", () => {
-                if (this.ancestor) this.ancestor.removeChild(this);
-            });
-            // disable further collisions while dying
-            if (this.body.setCollisionMask) this.body.setCollisionMask(0);
+            this.startDeath();
             return false;
         }
         if (other.body.collisionType === me.collision.types.PLAYER_OBJECT) {
-            // avoid physics pushback and resolve overlap by reverting enemy position
             if (typeof this._lastValidX === 'number' && typeof this._lastValidY === 'number') {
                 this.pos.x = this._lastValidX;
                 this.pos.y = this._lastValidY;
@@ -77,8 +91,8 @@ class EnemyEntity extends me.Sprite {
         const players = me.game.world.getChildByType(PlayerEntity);
         if (players && players.length > 0) {
             const player = players[0];
-            const targetX = player.getBounds().centerX;
-            const targetY = player.getBounds().centerY;
+            const targetX = player.getBounds().x;
+            const targetY = player.getBounds().y;
 
             const dx = targetX - this.getBounds().centerX;
             const dy = targetY - this.getBounds().centerY;
