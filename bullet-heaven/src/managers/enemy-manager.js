2d5dd1fa5c4121a8d9e86a335bfb67e1754ff265
emanuelpna <emanuelpna@yahoo.com.br>
2025-11-17 12:30:08 -0300

    atualiza EnemyManager para pegar a nova classe de zumbi

--- bullet-heaven/src/managers/enemy-manager.js
+++ bullet-heaven/src/managers/enemy-manager.js
@@ -1,11 +1,8 @@
 import * as me from 'melonjs';
 
-import EnemyEntity from './../renderables/enemy.js';
+import { ZombieAxe } from '../renderables/enemies/zombie-axe.js';
 
 class EnemyManager extends me.Container {
-    static COLS = 9;
-    static ROWS = 4;
-
     constructor() {
         super(0, 0, me.game.viewport.width, me.game.viewport.height);
 
@@ -23,7 +20,7 @@ class EnemyManager extends me.Container {
         for (let i = 0; i < initialCount; i++) {
             const x = Math.random() * (me.game.viewport.width - 32) + 16;
             const y = Math.random() * (me.game.viewport.height - 32) + 16;
-            const enemy = new EnemyEntity(x, y);
+            const enemy = new ZombieAxe(x, y);
             this.addChild(enemy);
         }
 
@@ -34,7 +31,7 @@ class EnemyManager extends me.Container {
         this.timer = me.timer.setInterval(() => {
             const x = Math.random() * (me.game.viewport.width - 32) + 16;
             const y = Math.random() * (me.game.viewport.height - 32) + 16;
-            const enemy = new EnemyEntity(x, y);
+            const enemy = new ZombieAxe(x, y);
             this.addChild(enemy);
         }, 4000);
     }
