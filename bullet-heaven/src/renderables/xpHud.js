6a6a32add7904742e16cdaaa3d8408c87eb40b98
emanuelpna <emanuelpna@yahoo.com.br>
2025-11-20 17:42:11 -0300

    atualiza XP HUD para adicionar sprite de moeda

--- bullet-heaven/src/renderables/xpHud.js
+++ bullet-heaven/src/renderables/xpHud.js
@@ -1,12 +1,21 @@
-// src/renderables/xpHud.js
 import * as me from "melonjs";
 import { GameData } from "../gameData.js";
 
-class XPHUD extends me.Text {
+class XPHUD extends me.Container {
     constructor() {
-        super(10, 10, {
-            font: "Arial",
-            size: 24,
+        super(0, 0, 200, 48);
+
+        this.floating = true;
+        this.alwaysUpdate = true;
+
+        this.coinIcon = new me.UISpriteElement(18, 18, {
+            image: me.loader.getImage("gold-coin")
+        });
+        this.addChild(this.coinIcon);
+
+        this.xpText = new me.Text(36, 11, {
+            font: "sans-serif",
+            size: 22,
             fillStyle: "#ffffff",
             textAlign: "left",
             textBaseline: "top",
@@ -28,14 +37,28 @@ class XPHUD extends me.Text {
     }
 
     update(dt) {
-        const newText = this.formatText();
-        if (newText !== this.lastText) {
-            this.lastText = newText;
-            this.setText(newText);
-            return true;
+        super.update(dt)
+
+        // só atualiza se o XP mudou
+        if (GameData.xp !== this.lastXP) {
+            this.lastXP = GameData.xp;
+            this.xpText.setText(String(this.lastXP));
+            return true; // precisa redesenhar
         }
         return false;
+
+        //  const newText = this.formatText();
+        // if (newText !== this.lastText) {
+        //     this.lastText = newText;
+        //     this.setText(newText);
+        //     return true;
+        //     this.goldText.setText("0");
+        //     this.addChild(this.goldText);
+
+        //     this.lastXP = -1;
+        // }
     }
+
 }
 
 export default XPHUD;
