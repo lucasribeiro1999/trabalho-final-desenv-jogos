3072ffc05e2580546187a32326107ea6e2a65cd1
emanuelpna <emanuelpna@yahoo.com.br>
2025-11-20 17:44:39 -0300

    adiciona sistema de hordas e troca para Stage de upgrades ao trocar de horda

--- bullet-heaven/src/game.js
+++ bullet-heaven/src/game.js
@@ -2,7 +2,9 @@ import * as me from 'melonjs';
 import { DebugPanelPlugin } from '@melonjs/debug-plugin';
 
 import PlayScreen from './stages/play.js';
-import GameOverScreen from './stages/gameOver.js';  // Import GameOver
+import GameOverScreen from './stages/gameOver.js';
+import { UpgradeSelectionScreen } from './stages/upgrade-selection.js';
+
 import Laser from './renderables/laser.js';
 import { GAME_RESOURCES } from './assets/resources';
 
@@ -11,13 +13,17 @@ export default function onload() {
         alert("Your browser does not support HTML5 canvas.");
         return;
     }
+
     me.plugin.register(DebugPanelPlugin, "debugPanel");
+
     me.audio.init("mp3,ogg");
+
     me.loader.setOptions({ crossOrigin: "anonymous" });
 
     me.loader.preload(GAME_RESOURCES, function () {
         me.state.set(me.state.PLAY, new PlayScreen());
-        me.state.set(me.state.GAMEOVER, new GameOverScreen()); // <-- Add
+        me.state.set(me.state.GAMEOVER, new GameOverScreen());
+        me.state.set(me.state.UPGRADE_SELECTION, new UpgradeSelectionScreen());
 
         me.pool.register("laser", Laser, true);
 
