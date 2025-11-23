7fa42c5868b6ee04221fa469096668849ebb785d
emanuelpna <emanuelpna@yahoo.com.br>
2025-11-23 17:33:38 -0300

    desenha a arma após pegar do drop do zombie

--- bullet-heaven/src/renderables/ui/weaponHud.js
+++ bullet-heaven/src/renderables/ui/weaponHud.js
@@ -28,13 +28,16 @@ class WeaponHud extends me.UISpriteElement {
     }
 
     drawWeapon(x, y) {
+        const thisWeaponLevel = GameData.weaponLevels[this.weaponType]
+
+        if (thisWeaponLevel === 0) return;
+
         const sprite = new me.UISpriteElement(x, y, {
             image: this.weaponType
         });
         sprite.scale(3)
         this.parent.addChild(sprite, 100);
 
-        const thisWeaponLevel = GameData.weaponLevels[this.weaponType]
         this.xpText = new me.Text(x + 16, y + 1, {
             font: "Micro 5",
             size: 25,
@@ -49,9 +52,19 @@ class WeaponHud extends me.UISpriteElement {
     }
 
     update(dt) {
+        const currentLevel = GameData.weaponLevels[this.weaponType];
+
+        // Se a arma foi desbloqueada (level > 0) mas ainda não desenhamos o texto/ícone
+        if (currentLevel > 0 && !this.xpText) {
+            // Precisamos redesenhar (chama drawWeapon usando a posição relativa correta)
+            // Nota: drawWeapon usa this.parent.addChild, então as coordenadas são relativas ao parent?
+            // O construtor chamou drawWeapon(x, y). Mas x e y não estão salvos na instância além de this.pos.
+            // O UISpriteElement tem this.pos.x e this.pos.y.
+            this.drawWeapon(this.pos.x, this.pos.y);
+        }
+
         // Update the weapon level text
         if (this.xpText) {
-            const currentLevel = GameData.weaponLevels[this.weaponType];
             this.xpText.setText(currentLevel);
         }
         return super.update(dt);
@@ -69,9 +82,11 @@ class WeaponHud extends me.UISpriteElement {
             const weaponLevel = GameData.weaponLevels[this.weaponType];
             let strokeColor = "yellow";
 
-            if (weaponLevel === 1) strokeColor = CONSTANTS.COLORS.BLUE;
-            else if (weaponLevel === 2) strokeColor = CONSTANTS.COLORS.PURPLE;
-            else if (weaponLevel >= 3) strokeColor = CONSTANTS.COLORS.ORANGE;
+            if (weaponLevel === 1) strokeColor = CONSTANTS.COLORS.GREEN;
+            else if (weaponLevel === 2) strokeColor = CONSTANTS.COLORS.BLUE;
+            else if (weaponLevel === 3) strokeColor = CONSTANTS.COLORS.PURPLE;
+            else if (weaponLevel === 4) strokeColor = CONSTANTS.COLORS.ORANGE;
+            else if (weaponLevel >= 5) strokeColor = CONSTANTS.COLORS.YELLOW;
 
             renderer.setColor(strokeColor);
             renderer.strokeRect(this.pos.x, this.pos.y, this.width, this.height);
