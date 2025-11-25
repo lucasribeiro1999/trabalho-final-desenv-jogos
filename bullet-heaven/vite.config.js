d799dbbb1bca574499d91101abadcb9c69a87ce1
emanuelpna <emanuelpna@yahoo.com.br>
2025-11-24 23:23:28 -0300

    remove arquivo do github pages que deu erradp

--- bullet-heaven/vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "vite";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    base: "https://lucasribeiro1999.github.io/trabalho-final-desenv-jogos/bullet-heaven/",
-});
